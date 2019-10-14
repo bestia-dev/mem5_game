d217358b86e1862eea7432f64f6d19b3b655f62b
Luciano <luciano.bestia@gmail.com>
2019-10-14 15:51:09 +0530

    service worker

--- webfolder/mem5/main.js
+++ webfolder/mem5/main.js
@@ -2,10 +2,10 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(function (registration) {
-            console.log('Service worker successfully registered on scope', registration.scope);
+        navigator.serviceWorker.register('sw.js').then(function (registration) {
+            sessionStorage.setItem("debug_text", "Service worker successfully registered on scope " + registration.scope + "\n" + sessionStorage.getItem("debug_text"));
         }).catch(function (error) {
-            console.log('Service worker failed to register', error);
+            sessionStorage.setItem("debug_text", "Service worker failed to register " + error + "\n" + sessionStorage.getItem("debug_text"));
         });
     });
 }
--- webfolder/mem5/sw.js
+++ webfolder/mem5/sw.js
@@ -1,19 +1,19 @@
 // sw.js
-var cacheName = 'WWW-EXAMPLE-COM-V1';
+var cacheName = 'mem5_game';
 var filesToCache = [
-    '/',                // index.html
-    '/main.js',
-    '/css/mem5.css',
-    '/images/icons-16.png',
-    '/images/icons-32.png',
-    '/images/icons-192.png',
-    '/images/icons-512.png'
+    'main.js',
+    'css/mem5.css',
+    'images/icons-16.png',
+    'images/icons-32.png',
+    'images/icons-192.png',
+    'images/icons-512.png'
 ];
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(cacheName)
             .then(function (cache) {
-                console.info('[sw.js] cached all files');
+                //serviceWorker cannot access SessionStorage or LocalStorage
+                console.log('[sw.js] cached all files');
                 return cache.addAll(filesToCache);
             })
     );
